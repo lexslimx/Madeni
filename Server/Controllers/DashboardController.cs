51cdc7baa62d9f4d1f47c6bbbe27f90ac15221c5
alex <alex.km@live.com>
2022-07-02 16:30:47 +0300

    update

--- Server/Controllers/DashboardController.cs
+++ Server/Controllers/DashboardController.cs
@@ -26,14 +26,17 @@ namespace Madeni.Server.Controllers
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             if(userId == null)
             {
-                return new DashboardDto();
+                return new DashboardDto
+                {
+                    UserId = "no user"
+                };
             }
             var dashboardDto = new DashboardDto
             {
-                IncomeTotal = _context.Incomes.Where(i => i.UserId == userId).Sum(i => i.Amount),
-                ExpenseTotal = _context.Expenses.Where(i => i.UserId == userId).Sum(i => i.Amount),
-                LoanTotal = _context.Loans.Where(i => i.UserId == userId).Sum(i => i.Amount),
-                RepaymentsTotal = _context.Repayments.Where(i => i.UserId == userId).Sum(i => i.Amount),
+                IncomeTotal = _context.Incomes.Sum(i => i.Amount),
+                ExpenseTotal = _context.Expenses.Sum(i => i.Amount),
+                LoanTotal = _context.Loans.Sum(i => i.Amount),
+                RepaymentsTotal = _context.Repayments.Sum(i => i.Amount),
                 DefaultConnection = _configuration.GetConnectionString("DefaultConnection"),
                 UserId = userId
             };
