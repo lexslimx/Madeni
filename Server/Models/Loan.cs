521a83ce6bb1faa6be475dd4144b9766652c7d36
alex <alex.km@live.com>
2022-07-25 12:53:03 +0300

    added loan features

--- Server/Models/Loan.cs
+++ Server/Models/Loan.cs
@@ -2,11 +2,16 @@
 {
     public class Loan
     {
+        public Loan()
+        {
+            Repayments = new HashSet<Repayment>();
+        }
         public int Id { get; set; }
         public string? Name { get; set; }
         public decimal Amount { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime ProspectiveDate { get; set; }
         public string UserId { get; set; } = string.Empty;
+        public  virtual ICollection<Repayment> Repayments { get; set; }
     }
 }
