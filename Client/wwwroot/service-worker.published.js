ba81ae211943ec003a22c57bca7759c233bb3213
alex <alex.km@live.com>
2022-08-01 21:36:17 +0300

    bump cache version

--- Client/wwwroot/service-worker.published.js
+++ Client/wwwroot/service-worker.published.js
@@ -1,6 +1,6 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
-const CACHE_VERSION = 8;
+const CACHE_VERSION = 10;
 
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
