be6d73ecb25cbfbd69f85c9034944461ad4fd435
lexslimx <alex.km@live.com>
2023-04-26 22:00:21 +0300

    added goals and investments pages

---
+++ Shared/Dtos/GoalDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madeni.Shared.Dtos
+{
+    public class GoalDto
+    {
+        public GoalDto()
+        {
+            Investments = new List<InvestmentDto>();
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime TargetDate { get; set; }
+        public IEnumerable<InvestmentDto> Investments { get; set; }
+    }
+}
---
+++ Shared/Dtos/InvestmentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Madeni.Shared.Dtos
+{
+    public class InvestmentDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid GoalId { get; set; }
+       
+    }
+}
