24d2c18f58140f7e8e66d1f55f19781d9497a7bc
lexslimx <alex.km@live.com>
2023-04-27 18:41:08 +0300

    Added goals pages, amount to goals

---
+++ Server/Data/Migrations/20230427153534_add amount to goals investments.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Madeni.Server.Data.Migrations
+{
+    public partial class addamounttogoalsinvestments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Investments",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Goals",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Investments");
+
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Goals");
+        }
+    }
+}
--- Server/Models/Goal.cs
+++ Server/Models/Goal.cs
@@ -7,6 +7,7 @@
         }
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public decimal Amount { get; set; }
         public string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime TargetDate { get; set; }
