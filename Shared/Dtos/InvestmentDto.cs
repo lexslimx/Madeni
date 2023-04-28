b67cc0d4a1341759a77b8f1a5d867ef6cf181bbe
lexslimx <alex.km@live.com>
2023-04-28 09:54:03 +0300

    updates

--- Shared/Dtos/InvestmentDto.cs
+++ Shared/Dtos/InvestmentDto.cs
@@ -6,8 +6,9 @@
         public string Name { get; set; }
         public decimal Amount { get; set; }
         public string Description { get; set; }
+        public DateTime Date { get; set; }
         public Guid GoalId { get; set; }
         public string UserId { get; set; }
-
+        public string? GoalName { get; set; }
     }
 }
